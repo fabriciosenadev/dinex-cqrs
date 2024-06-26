c59dd347acca588a5b387f4b9f815422222878b7
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-06-26 09:25:50 -0300

    add global usings

--- src/2.AppService/Dinex.AppService/Usings.cs
+++ src/2.AppService/Dinex.AppService/Usings.cs
@@ -1,15 +1,19 @@
 ﻿global using System;
 global using System.Linq;
 global using System.Text;
+global using System.Net;
+global using System.Net.Mail;
 global using System.Threading.Tasks;
 global using System.IdentityModel.Tokens.Jwt;
 global using System.Security.Claims;
 
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Options;
+global using Microsoft.Extensions.Logging;
 global using Microsoft.IdentityModel.Tokens;
 
 global using MediatR;
 
 global using Dinex.Infra;
 global using Dinex.Core;
+
