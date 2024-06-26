1c0f84024e2e8b2b4724bedfb7974348273ef4d8
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-06-25 23:16:29 -0300

    add user command to create new

---
+++ src/2.AppService/Dinex.AppService/Commands/User/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Dinex.AppService;
+
+public class CreateUserCommand : IRequest<OperationResult<Guid>>
+{
+    public string FullName { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public required string ConfirmPassword { get; set; }
+}
---
+++ src/2.AppService/Dinex.AppService/Commands/User/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace Dinex.AppService;
+
+public class CreateUserCommandHandler : ICommandHandler, IRequestHandler<CreateUserCommand, OperationResult<Guid>>
+{
+    private readonly IUserRepository _userRepository;
+
+    public CreateUserCommandHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<OperationResult<Guid>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var result = new OperationResult<Guid>();
+
+        if (_userRepository.FindAsync(user => user.Email == request.Email).Result.Any())
+        {
+            result.AddError("Já existe um usuário com este E-mail");
+            return result;
+        }
+
+        var newUser = User.CreateUser(
+                fullName: request.FullName,
+                email: request.Email,
+                password: request.Password,
+                confirmPassword: request.ConfirmPassword);
+        if (!newUser.IsValid)
+        {
+            result.AddNotifications(newUser.Notifications);
+            return result;
+        }
+
+        await _userRepository.AddAsync(newUser);
+
+        result.SetData(newUser.Id);
+
+        return result;
+    }
+}
