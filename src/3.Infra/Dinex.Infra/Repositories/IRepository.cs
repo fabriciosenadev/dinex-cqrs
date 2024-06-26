89d549caa9d8d5b0477c23df8d1d856699c22d52
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-06-25 22:58:03 -0300

    add base repository

---
+++ src/3.Infra/Dinex.Infra/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Dinex.Infra;
+
+public interface IRepository<T> : IDisposable where T : Entity
+{
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task<T?> GetByIdAsync(Guid id);
+    Task<List<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entity);
+    Task UpdateRangeAsync(IEnumerable<T> entity);
+    Task SaveChangesAsync();
+}
---
+++ src/3.Infra/Dinex.Infra/Repositories/Repository.cs
@@ -0,0 +1,83 @@
+﻿namespace Dinex.Infra;
+
+public abstract class Repository<T> : IRepository<T> where T : Entity//, new()
+{
+    protected readonly DbSet<T> _dbSet;
+    protected readonly DinexApiContext _context;
+
+    protected Repository(DinexApiContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.AsNoTracking().Where(predicate).ToListAsync();
+    }
+
+    //public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    //{
+    //    var query = _dbSet.AsNoTracking().AsQueryable();
+    //    var filteredData = query.Where(predicate);
+
+    //    // Avaliação no lado do cliente
+    //    return await filteredData.ToListAsync();
+    //}
+
+
+    public virtual async Task<T?> GetByIdAsync(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public virtual async Task<List<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    //public virtual async Task<List<T>> GetPagedAsync(int pageNumber, int pageSize)
+    //{
+    //    return await _dbSet.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+    //}
+
+    public virtual async Task AddAsync(T entity)
+    {
+        _dbSet.Add(entity);
+        await SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteAsync(T entity)
+    {
+        _dbSet.Remove(entity);
+        await SaveChangesAsync();
+    }
+
+    public virtual async Task UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        await SaveChangesAsync();
+    }
+
+    public virtual async Task AddRangeAsync(IEnumerable<T> entity)
+    {
+        _dbSet.AddRange(entity);
+        await SaveChangesAsync();
+    }
+
+    public virtual async Task UpdateRangeAsync(IEnumerable<T> entity)
+    {
+        _dbSet.UpdateRange(entity);
+        await SaveChangesAsync();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context?.Dispose();
+    }
+}
