fca18cbaef8244ef3d1cd6535a7754c5fbfe6b20
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-06-25 23:17:09 -0300

    add endpoints to create and get user

---
+++ src/1.WebApi/Dinex.Api/V1/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Dinex.Api.Controllers;
+
+namespace Dinex.Api.V1.Controllers;
+
+[ApiVersion("1.0")]
+[Route("v{version:apiVersion}/[controller]")]
+public class UsersController : MainController
+{
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(CreateUserCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return HandleResult(result);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Get([FromQuery] GetUserQuery request)
+    {
+        var result = await _mediator.Send(request);
+        return HandleResult(result);
+    }
+}
