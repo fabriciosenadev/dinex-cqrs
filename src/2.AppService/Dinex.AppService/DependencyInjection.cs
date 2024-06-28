fecf409fe8b0436d4dba8409b77e1aeed4af9823
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-06-28 00:24:02 -0300

    chnages for DI

--- src/2.AppService/Dinex.AppService/DependencyInjection.cs
+++ src/2.AppService/Dinex.AppService/DependencyInjection.cs
@@ -1,10 +1,12 @@
-﻿namespace Dinex.AppService;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Dinex.AppService;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection RegisterBusinessDependecies(this IServiceCollection services)
+    public static IServiceCollection RegisterBusinessDependecies(this IServiceCollection services, IConfiguration configuration)
     {
-        services.RegisterInfraDependencies();
+        services.RegisterInfraDependencies(configuration);
 
         #region service
         services.AddScoped<IAccountService, AccountService>();
--- src/3.Infra/Dinex.Infra/DependencyInjection.cs
+++ src/3.Infra/Dinex.Infra/DependencyInjection.cs
@@ -1,10 +1,18 @@
-﻿namespace Dinex.Infra;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Dinex.Infra;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection RegisterInfraDependencies(this IServiceCollection services)
+    public static IServiceCollection RegisterInfraDependencies(this IServiceCollection services, IConfiguration configuration)
     {
+        //connection string
+        services.AddDbContext<DinexApiContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DinexDB"))
+        );
+
         services.AddTransient<IUserRepository, UserRepository>();
+        services.AddScoped<IQueueInRepository, QueueInRepository>();
 
         return services;
     }
