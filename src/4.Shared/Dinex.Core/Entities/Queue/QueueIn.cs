df9a5740b763a6ccd22581649eb238c16a91bd0f
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-06-28 18:38:55 -0300

    add constructor

--- src/4.Shared/Dinex.Core/Entities/Queue/QueueIn.cs
+++ src/4.Shared/Dinex.Core/Entities/Queue/QueueIn.cs
@@ -6,17 +6,32 @@ public class QueueIn : Entity
     public TransactionActivity Type { get; private set; }
     public string FileName { get; private set; }
 
+    private QueueIn (
+        Guid userId, 
+        TransactionActivity type, 
+        string fileName,
+        DateTime createdAt,
+        DateTime? updatedAt,
+        DateTime? deletedAt)
+    {
+        UserId = userId;
+        Type = type;
+        FileName = fileName;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+        DeletedAt = deletedAt;
+    }
     public static QueueIn Create(Guid userId, TransactionActivity transactionActivity, string fileName)
     {
         var filePrefix = Guid.NewGuid();
 
-        var newQueueIn = new QueueIn()
-        {
-            UserId = userId,
-            Type = transactionActivity,
-            FileName = $"{filePrefix}_{fileName}",
-            CreatedAt = DateTime.UtcNow,
-        };
+        var newQueueIn = new QueueIn(
+            userId,
+            type: transactionActivity,
+            fileName: $"{filePrefix}_{fileName}",
+            createdAt: DateTime.UtcNow,
+            updatedAt: null,
+            deletedAt: null);
 
         return newQueueIn;
     }
