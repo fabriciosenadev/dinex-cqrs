5f1a48b7a4d369bf6aeb6e3edc1cdd2c05bd0c6f
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-06-28 00:23:19 -0300

    add queue in repository

---
+++ src/3.Infra/Dinex.Infra/Repositories/queue/QueueInRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Dinex.Infra;
+
+public interface IQueueInRepository : IRepository<QueueIn>
+{   
+}
+
+internal class QueueInRepository : Repository<QueueIn>, IQueueInRepository
+{
+    public QueueInRepository(DinexApiContext context) : base(context)
+    {
+    }
+}
