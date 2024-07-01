a61a63bf6827f6166bc182ac06efc338033cc4cb
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-06-30 21:33:31 -0300

    add auth command

---
+++ src/2.AppService/Dinex.AppService/Commands/User/LoginUser/LoginUserCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace Dinex.AppService;
+
+public class LoginUserCommand : IRequest<OperationResult<AuthenticationUserDTO>>
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ src/2.AppService/Dinex.AppService/Commands/User/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿namespace Dinex.AppService;
+
+public class LoginUserCommandHandler : ICommandHandler, IRequestHandler<LoginUserCommand, OperationResult<AuthenticationUserDTO>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IAccountService _accountService;
+
+    public LoginUserCommandHandler(IUserRepository userRepository, IAccountService accountService)
+    {
+        _userRepository = userRepository;
+        _accountService = accountService;
+    }
+
+    public async Task<OperationResult<AuthenticationUserDTO>> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+    {
+		try
+		{
+            var result = new OperationResult<AuthenticationUserDTO>();
+
+            var user = (await _userRepository.FindAsync(x => x.Email == request.Email)).FirstOrDefault();
+            if (user is null)
+            {
+                result.AddError("Usuário não encontrado").SetAsNotFound();
+                return result;
+            }
+
+            if (user.UserStatus == UserStatus.Inactive)
+            {
+                result.AddError("Conta inativa");
+                return result;
+            }
+
+            if (!User.MatchValues(user.Password, request.Password))
+            {
+                result.AddError("Login ou senha incorretos");
+                return result;
+            }
+
+            var token = _accountService.GenerateToken(user);
+            result.SetData(new AuthenticationUserDTO(user, token));
+            return result;
+
+        }
+        catch (Exception ex)
+		{
+            return new OperationResult<AuthenticationUserDTO>()
+                .SetAsInternalServerError()
+                .AddError($"An unexpected error ocurred to method: AuthenticateAsync {ex}");
+        }
+    }
+}
