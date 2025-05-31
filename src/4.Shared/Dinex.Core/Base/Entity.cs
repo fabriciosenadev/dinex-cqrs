d468611cce2c5b0cadfaf942484555890ef68413
Fabricio Sena <fabricio.sena@eaibrasil.com>
2025-05-31 13:32:11 -0300

    add controle de entidade para deleção

--- src/4.Shared/Dinex.Core/Base/Entity.cs
+++ src/4.Shared/Dinex.Core/Base/Entity.cs
@@ -11,4 +11,20 @@ public abstract class Entity : Notifiable<Notification>
     {
         Id = Guid.NewGuid();
     }
+
+    public void MarkAsDeleted()
+    {
+        DeletedAt = DateTime.UtcNow;
+    }
+
+    public bool IsDeleted()
+    {
+        return DeletedAt.HasValue;
+    }
+
+    public void EnsureNotDeleted(string entityName)
+    {
+        if (IsDeleted())
+            AddNotification(entityName, $"A entidade {entityName} foi excluída e não pode mais ser utilizada.");
+    }
 }
